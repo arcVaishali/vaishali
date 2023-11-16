a6fb08c5eb54fd9049846a25287e302684e61331
vaishali <vaishalirdj14@gmail.com>
2023-11-16 14:32:12 +0530

    footer added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className="grid grid-cols-12 p-32 mt-8 bg-[#121212] text-white">
       <div
         className="flex flex-col text-8xl font-black justify-center items-center col-start-3 col-end-11 uppercase"
-        style={{ fontFamily: "monospace" }}
+        style={{ fontFamily: "Fontsfree net xillian, sans-serif" }}
       >
         <span className="text-center m-2">Have an Idea?</span>
         <button className="rounded-full uppercase py-4 px-8 border-[1px] border-white hover:bg-[#e4eae4da] hover:text-black">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const sitemap = [
+    { name: "home", to: "/" },
+    { name: "about", to: "/about" },
+    { name: "work", to: "/work" },
+    { name: "blog", to: "/blog" },
+    { name: "profile", to: "/profile" },
+    { name: "contact", to: "/contact" },
+  ];
 
-export default Footer
+  const socials = [
+    { name: faInstagram, to: "https://www.instagram.com/vaishali_p14" },
+    { name: faLinkedin, to: "https://www.instagram.com/vaishali_p14" },
+    { name: faTwitter, to: "https://www.instagram.com/vaishali_p14" },
+    // {to:"https://www.instagram.com/vaishali_p14"},
+    // {to:"https://www.instagram.com/vaishali_p14"},
+  ];
+  return (
+    <div className="grid grid-cols-12 col-span-12 px-16 py-8 justify-between items-center bg-[#121212] border-t-[1px] border-b-[1px] border-gray-700 text-white">
+      <div className="col-span-3">Vaishali</div>
+      <div className="flex col-span-6 justify-center items-center">
+        {sitemap.map((element, index) => (
+          <Link
+            to={element.to}
+            className="font-thin uppercase mx-4 text-xs hover:text-gray-300"
+          >
+            {element.name}
+          </Link>
+        ))}
+      </div>
+      <div className="flex col-span-3 justify-center items-center">
+        {socials.map((element, index) => (
+          <Link
+            to={element.to}
+            className="font-thin uppercase mx-4 text-xs hover:text-gray-300"
+          >
+            {" "}
+            <FontAwesomeIcon icon={element.name} />{" "}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
