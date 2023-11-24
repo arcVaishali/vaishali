0b9f77ea379bbc472c5f060a83187b1b40841453
vaishali <vaishalirdj14@gmail.com>
2023-11-25 00:13:20 +0530

    modified

--- src/components/About/Hero.js
+++ src/components/About/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   const names = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 0];
   const imgg = [1, { one: "", two: "" }, 3, 4];
   return (
-    <div className="grid grid-cols-12 justify-center items-center p-32">
+    <div className="grid grid-cols-12 justify-center items-center pt-32 pl-32 pr-32 pb-8 mb-16">
       <div className="grid grid-cols-6 col-span-12 justify-center items-center">
         <div className="col-span-6 text-7xl text-center uppercase font-black p-2">
           {" "}
@@ -15,7 +15,7 @@ const Hero = () => {
           domination. At the end of the day, going forward, a new.
         </div>
       </div>
-      <div className="col-span-12 grid grid-cols-12 justify-center items-center p-16 gap-8">
+      <div className="col-span-12 grid grid-cols-12 justify-center items-center p-8 gap-8">
         {imgg.map((element, index) => (
           <div className="col-span-3">
             {index === 1 ? (
@@ -26,7 +26,7 @@ const Hero = () => {
                     className="w-32 h-fill"
                   />
                 </div>
-                <div className="relative left-32 col-span-1">
+                <div className="relative z-10 left-32 col-span-1">
                   <img
                     src="https://assets.website-files.com/643d8b145b9de160a97cf566/64420e1e621d9f5fb6e41cd5_Rectangle%2041189.jpg"
                     className="w-32 h-fill"
--- src/components/About/Slider.js
+++ src/components/About/Slider.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Slider from "@benavente/slider"
 
 const Slider = () => {
   return (
-    <div className="grid grid-cols-12 col-span-12 justify-center items-center p-32">
-      <span className="col-span-12 text-7xl uppercase font-black">
+    <div className="grid grid-cols-12 col-span-12 justify-center items-center p-8">
+      <span className="col-span-12 text-6xl uppercase font-black col-start-1 text-center">
         What to expect from me
       </span>
-      {/* PROJECT SLIDER */}
+      
     </div>
   );
 };
