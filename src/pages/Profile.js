6f2f07d2ae8daf5bae457523b9852ad3e3237ce9
vaishali <vaishalirdj14@gmail.com>
2023-11-30 22:49:04 +0530

    socials and profile added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
+    <div className='grid grid-cols-12 col-span-12 p-32 m-8'>
+      <span className='col-span-12 text-5xl uppercase'>Socials and Profiles</span>
+      <div className='grid grid-cols-12 col-span-12'>
+        
+      </div>
       
     </div>
   )
