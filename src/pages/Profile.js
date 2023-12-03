e0c262299b1d4f38f9aaac2f9f4e918a9ef2cf2c
vaishali <vaishalirdj14@gmail.com>
2023-12-03 21:25:50 +0530

    profile added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
+  const list = [
+    {
+      name: "LinkedIn",
+      src: "https://www.linkedin.com/vaishali_p14",
+      img: "",
+    },
+  ];
   return (
-    <div className='grid grid-cols-12 col-span-12 p-32 m-8'>
-      <span className='col-span-12 text-5xl uppercase'>Socials and Profiles</span>
-      <div className='grid grid-cols-12 col-span-12'>
-        
+    <div className="grid grid-cols-12 col-span-12 p-32 m-8">
+      <span className="col-span-12 text-7xl uppercase font-black">
+        Socials and Profiles
+      </span>
+      <div className="grid grid-cols-12 col-span-12 justify-center items-center">
+        <div className="grid grid-cols-6 col-span-6">
+          {list.map((element, index) => (
+            <div className="flex flex-row col-span-3 items-center">
+              <div className="w-4">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png"
+                  className="w-fill h-fill"
+                />
+              </div>
+              <span className="text-base text-black font-extralight">
+                {element.name}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
