4c3b9ca91509206a4f4e0ac1b36457a8435ac918
vaishali <vaishalirdj14@gmail.com>
2023-12-28 00:30:43 +0530

    profile banner created

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,10 +5,15 @@ import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-  const list = [ 
-    {icon:faInstagramSquare , color:"pink"}, {icon:faLinkedin , color:"blue"}, {icon:faTwitter , color:"lightblue"}, {icon:faGithub , color:"black"}];
+  const list = [
+    { icon: faInstagramSquare, color: "pink" },
+    { icon: faLinkedin, color: "blue" },
+    { icon: faTwitter, color: "lightblue" },
+    { icon: faGithub, color: "black" },
+  ];
 
   const list1 = [
     {
@@ -33,24 +38,32 @@ const Profile = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-12 col-span-12 px-32 py-16 m-8 justify-center items-center">
-      <img src={Picture} className="col-span-2 h-[200px] w-[400px]" />
+    <div className="grid grid-cols-12 col-span-12 m-8 justify-center items-center">
+      <img className="col-span-12" src="https://static.vecteezy.com/system/resources/previews/002/558/855/original/abstract-dark-black-and-grey-modern-material-header-technology-banner-abstract-widescreen-background-vector.jpg"/>
+      <img src={Picture} className="absolute ml-8 top-[300px] col-span-2 h-[200px] w-[200px] rounded-[800px]" />
 
       <div className="grid grid-cols-12 col-span-12 justify-between items-center m-8">
-        <span className="col-span-4 text-xl" style={{fontFamily:"fantasy"}}>
-          Vaishali
+        <span className="col-span-4 leading-3">
+          <div className="text-xl" style={{ fontFamily: "fantasy" }}>
+            Vaishali
+          </div>
+          <Link className="underline text-[12px] font-extralight italic text-gray-500" to="/">
+            Check out
+          </Link>
         </span>
-        <div className="flex justify-end items-end col-span-8 ">
+        <div className="flex justify-end items-end col-span-8 mb-2">
           {list.map((element, key) => (
             <FontAwesomeIcon
               icon={element.icon}
               color={element.color}
               size="lg"
-              className="bg-white p-2"
+              className="bg-white m-2 rounded-full shadow-md p-2"
             ></FontAwesomeIcon>
           ))}
         </div>
+      <div className="font-extralight text-sm col-span-12 text-gray-500">Mollit ut velit eiusmod pariatur Lorem non amet est.Nostrud in tempor irure id adipisicing anim.</div>
       </div>
+
     </div>
   );
 };
