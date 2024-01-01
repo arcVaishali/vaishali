0beb465e21fb00bf6651e34f28eb3be76b93dfa6
vaishali <vaishalirdj14@gmail.com>
2024-01-01 18:56:14 +0530

    modified

--- src/components/Blog/Home.js
+++ src/components/Blog/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='grid grid-cols-12 col-span-12 justify-center items-center p-32'>
-     <span className="col-span-10 text-7xl uppercase font-black">
+     <span className="col-span-10 col-start-2 text-7xl uppercase font-black">
           Blogs and Articles
         </span>
     </div>
--- src/components/Work/Latest.js
+++ src/components/Work/Latest.js
@@ -23,7 +23,7 @@ const Latest = () => {
   ];
   return (
     <div className="grid grid-cols-12 justify-center items-center px-16">
-      <span className="col-span-12 col-start-2 text-7xl uppercase font-black p-16 m-8">
+      <span className="col-span-12 col-start-2 ml-16 text-7xl uppercase font-black p-16 m-8">
         Latest Projects
       </span>
       <div className="grid grid-cols-12 col-span-12 gap-16 justify-center items-center">
@@ -36,7 +36,7 @@ const Latest = () => {
             <div className="z-8 rotate-6">
               <img src={element.image} className="w-fill h-fill" />
             </div>
-            <span className="relative -left-4 bg-black text-white text-l uppercase z-9 p-2 rotate-6">
+            <span className="relative -left-[20px] bg-black text-white text-l uppercase z-9 p-2 rotate-6">
               {element.name}
             </span>
             {/* </div> */}
