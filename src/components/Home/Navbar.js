ecee5d777df3d623352b901bdf4367fa8f1e7a10
vaishali <vaishalirdj14@gmail.com>
2024-01-24 21:38:03 +0530

    navbar modified

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
           </animated.div>
           <animated.div
             style={{ ...spring }}
-            className="relative right-32 max-[1023px]:flex max-[1023px]:col-span-1 max-[1023px]:justify-end max-[1023px]:col-start-10 min-[1024px]:hidden"
+            className="flex col-span-1 justify-end col-start-9 lg:hidden"
           >
             <FontAwesomeIcon
               className="text-4xl cursor-pointer"
