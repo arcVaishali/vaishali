938e29904aa0d579360c5cff0662b06060fc57f9
vaishali <vaishalirdj14@gmail.com>
2024-01-25 00:21:55 +0530

    blog page made responsive

--- src/components/Blog/Home.js
+++ src/components/Blog/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div data-aos="zoom-in" className='grid grid-cols-12 col-span-12 justify-center items-center p-32'>
-     <span className="col-span-10 col-start-2 text-7xl uppercase font-black">
+    <div data-aos="zoom-in" className='grid grid-cols-12 col-span-12 justify-center items-center py-16 px-16 lg:p-32'>
+     <span className="col-span-10 ml-16 lg:col-start-2 text-4xl lg:text-7xl uppercase font-black">
           Blogs and Articles
         </span>
     </div>
